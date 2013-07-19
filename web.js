3126b3aceca726f2a4da6d0381533bdaeead5c4e
Charlie <ecirmada@gmail.com>
2013-07-19 15:31:44 +1000

    index addition

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs);
+
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('erm yes?');
+ var indexbuffer = fs.readFileSync('index.html');
+ var str = indexbuffer.toString('utf-8');
+
+
+ response.send(str);
 });
 
 var port = process.env.PORT || 5000;
